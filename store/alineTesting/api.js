917a2b5e1fc386c50e2197fdab6132477ce84e16
Crowdbotics <engineering@crowdbotics.com>
2023-05-05 15:03:16 +0000

    Update store.

---
+++ store/alineTesting/api.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+const alineTesting = axios.create({
+  baseURL: "https://hello.com/api/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function alinetesting_delete_users_id_delete(payload) {
+  return alineTesting.delete(`/users/${payload.id}/`)
+}
+export const apiService = { alinetesting_delete_users_id_delete }
