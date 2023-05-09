eb3c3472a18787ff40d63a2cc62642b082b2956b
Aline Tavares <aline@crowdbotics.com>
2023-05-09 13:30:27 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- store/alineTesting/api.js
+++ store/alineTesting/api.js
@@ -1,9 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 const alineTesting = axios.create({
   baseURL: "https://hello.com/api/",
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json"
+  }
+});
+
 function alinetesting_delete_users_id_delete(payload) {
-  return alineTesting.delete(`/users/${payload.id}/`)
+  return alineTesting.delete(`/users/${payload.id}/`);
 }
-export const apiService = { alinetesting_delete_users_id_delete }
+
+export const apiService = {
+  alinetesting_delete_users_id_delete
+};
